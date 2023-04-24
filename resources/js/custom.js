f0d4c379fe1614420361a9a91e9d15a30d239853
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-04-24 20:22:00 +0200

    added custom js

---
+++ resources/js/custom.js
@@ -0,0 +1,20 @@
+// Check local storage for the user's preference
+const isDarkMode = localStorage.getItem('darkMode') === 'true';
+
+// Set the initial state of the toggle based on the user's preference
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+if (isDarkMode) {
+  toggleSwitch.checked = true;
+  document.documentElement.setAttribute('data-theme', 'dark');
+}
+
+// Listen for changes to the toggle and update local storage and the page accordingly
+toggleSwitch.addEventListener('change', function(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('darkMode', 'true');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('darkMode', 'false');
+  }
+});
