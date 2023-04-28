0fa52da98c261019d2f0cd5a19e2283283cde6a1
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-04-28 12:20:41 +0200

    basic setup
    setup basic structur front and backend

---
+++ database/migrations/2023_04_28_100608_create_messages_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+};
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                    <a href="{{ url('/chat') }}">Chat</a>
+                    {{ __("You're logged in!") }}<br>
+                    <a href="{{ url('/playground') }}">Playground</a>
                     <div class="container">
                         <div class="row">
                             <div class="col bg-info">
