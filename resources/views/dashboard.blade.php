b9cf95a725c76724bbfd0850f4aa1ca639bd4b24
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-04-29 23:52:59 +0200

    varos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="{{-- font-semibold text-xl text-gray-800 leading-tight --}}">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
@@ -11,16 +11,42 @@
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}<br>
                     <a href="{{ url('/playground') }}">Playground</a>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col bg-info">
-                            test
-                            <button class="btn btn-warning">Danger danger...High Voltage</button>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col bg-info">
+                                test
+                                <button class="btn btn-warning">Danger danger...High Voltage</button>
+                                </div>
                             </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!---------------------------------------------------------------- TEst edit tailwind components -->
+    <div class="container-fluid bg-info">
+        <div class="row">
+            <div class="col">
+                <h2 class="{{-- font-semibold text-xl text-gray-800 leading-tight --}}">
+                    {{ __('Dashboard') }}
+                </h2>
+            </div>
+            <div class="col">
+        {{ __("You're logged in!") }}<br>
+            <a href="{{ url('/playground') }}">Playground</a>
+                <div class="container">
+                    <div class="row">
+                        <div class="col bg-info">
+                        test
+                        <button class="btn btn-warning">Danger danger...High Voltage</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+<!---------------------------------------------------------------- TEst edit tailwind components -->
 </x-app-layout>
+
+
