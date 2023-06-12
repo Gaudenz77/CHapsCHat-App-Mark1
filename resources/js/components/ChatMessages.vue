cc27819bbeaf790889de3fd4c5e27462af212b1c
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-06-12 19:31:18 +0200

    update

--- resources/js/components/ChatMessages.vue
+++ resources/js/components/ChatMessages.vue
@@ -12,8 +12,8 @@
         </figcaption>
         <div class="text-end">
           <button v-if="message.user.id === authUserId" class="btn btn-danger btn-sm me-2 text-end" @click="deleteMessage(message.id)">
-            Delete
-          </button>
+          Delete
+        </button>
         </div>
       </div>
       <div class="dividerone"></div>
@@ -22,6 +22,7 @@
 </template>
 
 <style>
+
 .dividerone {
   height: 2px;
   background-color: black;
@@ -30,11 +31,17 @@
 </style>
 
 <script>
+
 import axios from 'axios';
 import Echo from 'laravel-echo';
 
 export default {
   props: ['authUserId', 'messages'],
+  data() {
+    return {
+      messages: [],
+    };
+  },
   computed: {
     reversedMessages() {
       return this.messages.slice().reverse();
@@ -62,15 +69,13 @@ export default {
           message: e.message.message,
           user: e.user
         });
-      }).listenForWhisper('subscription_succeeded', (e) => {
-        console.log('Subscription succeeded:', e);
       });
     } catch (error) {
       console.error('Error initializing Echo:', error);
     }
   },
   mounted() {
-    axios.get('/messages').then((response) => {
+    axios.get("/messages").then((response) => {
       this.messages = response.data.reverse();
     });
   },
@@ -86,28 +91,28 @@ export default {
       this.messages.unshift(message);
     },
     deleteMessage(messageId) {
-      if (typeof messageId !== 'undefined') {
-        // If the message has an ID, it is saved in the database
-        axios.delete(`/messages/${messageId}`)
-          .then(response => {
-            // Handle successful deletion
-            console.log('Message deleted:', response.data);
-            // Find the index of the message in the messages array
-            const index = this.messages.findIndex(message => message.id === messageId);
-            // Remove the message from the messages array
-            if (index !== -1) {
-              this.messages.splice(index, 1);
-            }
-          })
-          .catch(error => {
-            // Handle deletion error
-            console.error('Error deleting message:', error);
-          });
-      } else {
-        // If the message doesn't have an ID, it is unsaved and can be directly removed from the messages array
-        this.messages = this.messages.filter(message => message.id !== undefined);
-      }
-    }
+  if (typeof messageId !== 'undefined') {
+    // If the message has an ID, it is saved in the database
+    axios.delete(`/messages/${messageId}`)
+      .then(response => {
+        // Handle successful deletion
+        console.log('Message deleted:', response.data);
+        // Find the index of the message in the messages array
+        const index = this.messages.findIndex(message => message.id === messageId);
+        // Remove the message from the messages array
+        if (index !== -1) {
+          this.messages.splice(index, 1);
+        }
+      })
+      .catch(error => {
+        // Handle deletion error
+        console.error('Error deleting message:', error);
+      });
+  } else {
+    // If the message doesn't have an ID, it is unsaved and can be directly removed from the messages array
+    this.messages = this.messages.filter(message => message.id !== undefined);
+  }
+}
   }
 };
 </script>
