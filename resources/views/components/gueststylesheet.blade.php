85291550c97a22412ab1e5feba10c53daa6ca778
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-06-14 13:06:03 +0200

    updat

---
+++ resources/views/components/gueststylesheet.blade.php
@@ -0,0 +1,126 @@
+<style>
+    body {
+          /* background-color: black; */
+    }
+
+    @media (max-width: 565.98px) {
+      .loginMain {
+        margin-top:36vh;
+        margin-bottom:18vh;
+      }
+
+      .registerMain {
+        margin-top:30vh;
+        margin-bottom:16vh;
+      }
+
+      .welcomeMain {
+        margin-top:19rem;
+        margin-bottom:8rem;
+      }
+
+      .blogMain {
+      margin-top:22vh;
+      margin-bottom:18vh;
+      }
+
+      .aboutMain {
+      margin-top:22vh;
+      margin-bottom:16vh;
+      }
+
+      .navbar {
+        height: 18rem;
+        background-color:#091540;
+      }
+
+      .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        height: 24%;
+        width: 100%;
+        margin-top: 0;
+      }
+
+      .colTriangle {
+        left: 52%;
+      }
+
+      .navbar-toggler {   
+      /* font-size:20px; */
+      /*   margin-left:0rem;
+        margin-right: 0rem;  */
+      }
+
+      .logoImg {
+        width:110%;
+        margin-left:2rem;
+        padding-left:1rem;
+        position:relative;
+        left:0;
+        justify-content:center;
+      }
+
+      .wrapper {
+        width: 14vh;
+        padding-right: 2rem;
+      }
+
+      a.myNavToggleTitle {
+        color: #333;
+      }
+    }
+
+    @media (min-width: 566px) {
+      .loginMain {
+       margin-top:14rem;
+      }
+
+      .registerMain {
+        margin-top:8%;
+        margin-bottom:-50%;
+      }
+
+      .blogMain {
+      margin-top:14vh;
+      margin-bottom:11vh;
+      }
+
+      .aboutMain {
+      margin-top:35vh;
+      margin-bottom:18vh;
+      }
+
+      .aboutCol{
+        height:40vh;
+      }
+
+      .wrapper {
+        width: 20vh;
+        padding-right: 2rem;
+      }
+
+
+    }
+     
+    .custom-toggler.navbar-toggler {
+          border-color: rgb(255, 255, 0);
+    }
+     
+    .custom-toggler .navbar-toggler-icon {
+          background-image: url(
+          "data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 0, 0.8)' stroke-width='3' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+    }
+
+    body.dark-mode .custom-toggler.navbar-toggler {
+          border-color: rgb(255, 0, 0);
+          background-color:#ebebeb;
+          color: #ebebeb;
+    }
+
+      body.dark-mode .custom-toggler .navbar-toggler-icon {
+          background-image: url(
+          "data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 0, 0, 0.8)' stroke-width='3' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+    }
+  </style>
