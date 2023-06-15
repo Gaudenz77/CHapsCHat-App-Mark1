f4e141dd680e613345d1c1eebcbbc6619bd1d5f8
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-06-15 19:30:36 +0200

    go

--- resources/views/playground.blade.php
+++ resources/views/playground.blade.php
@@ -7,12 +7,13 @@
 @section('content')
 
 @auth
+
 <section class="">
   <div class="container-fluid">
 
     <div class="row">
 
-      <div class="chatCol col-md-3 px-4 mt-2">
+      <div class="chatCol col-md-3 px-4 mt-2 pb-4">
         <div class="mt-2 p-1 pt-4">
           <h5>WELCOME to the MESSAGECORNER</h5>
           <div class="chatFormField mt-5 px-0">
@@ -25,15 +26,27 @@
        <p class="test text-center mb-0 m-2 pt-5">Messageservice brought to you by<a href="https://pusher.com/">Pusher&copy;</a></p>
       </div>
 
-      <div class="editorCol col-md-5 px-4 mt-2">
+      <div class="editorCol col-md-5 px-4 mt-2 pb-4">
           <div class="mt-2 p-1 pt-4">
-            <h5 class="mb-3 pb-3">ThiS iS Your EdiTor!</h5>
+            <h5 class="mb-0 pb-0">ThiS iS Your EdiTor!</h5>
+            <div class="row text-end">
+              <div class="col">
+                <div class="btn-group" role="group" aria-label="Basic example">
+                  <button  id="undoButton" type="button" class="btn btn-danger">Undo</button>
+                  <button  id="redoButton" type="button" class="btn btn-success">Redo</button>
+                </div>
+              </div>
+
+            </div>
+
             <div id="editor" style="height:75vh;"></div>
+            
           </div>
       </div>
 
-      <div class="previewCol col-md-4 px-4 mt-2">
+      <div class="previewCol col-md-4 px-4 mt-2 pb-4">
           <div class="mt-2 p-1 pt-4">
+
             <h5 class="mb-3 pb-3">YouR ouTpuT!</h5>
               <iframe id="preview" style="height:75vh;"></iframe>
           </div>
@@ -66,18 +79,13 @@
 
 @endauth
 
+<script src="https://unpkg.com/monaco-editor@0.37.0/min/vs/loader.js"></script>
 <script>
-  
-// FUNCTIONAL HTMML ONLY START --------------------------------
-
-// script Monaco in-site editor/preview --------------------------------
-
-require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.37.0/min/vs' }});
+  require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.37.0/min/vs' }});
   require(['vs/editor/editor.main'], function() {
     monaco.editor.defineTheme('dark-theme', {
       base: 'vs-dark',
       inherit: true,
-
       rules: [
         { token: 'comment', foreground: '7f7f7f' },
         { token: 'delimiter', foreground: 'd4d4d4' },
@@ -87,52 +95,74 @@ require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.37.0/min/vs' 
         { token: 'tag', foreground: '569cd6' },
         { token: 'attribute.name', foreground: '9cdcfe' },
         { token: 'attribute.value', foreground: 'ce9178' },
-        { token: 'operator', foreground: 'd4d4d4' },
+        { token: 'operator', foreground: 'd4d4d4' }
       ],
       colors: {
         'editor.background': '#1e1e1e',
-        'editor.foreground': '#d4d4d4',
+        'editor.foreground': '#d4d4d4'
       }
-      
     });
-          
-// localstorage insite editor for keep code when reload page,  Monaco in-site editor/preview --------------------------------
- 
-          var editorValue = localStorage.getItem('editorValue');
-          var editor = monaco.editor.create(document.getElementById('editor'), {
-          value: editorValue || '<!DOCTYPE html>\n<html>\n<head>\n  <meta charset="UTF-8">\n  <title>My Test Page</title>\n<style>body{color:white;}</style>\n</head>\n<body> \n</body>\n</html>',
-          language: 'html',
-          theme: 'dark-theme',
-          automaticLayout: true ,
-          suggestOnTriggerCharacters: true, // enable auto-completion on trigger characters
-          autoClosingBrackets: true, // enable auto-closing brackets
-          autoClosingQuotes: true, // enable auto-closing quotes
-          autoIndent: true, // enable auto-indent
-          snippets: [{
-            label: 'HTML5 Template', // HTML5 template snippet
-            body: '<!DOCTYPE html>\n<html>\n<head>\n  <meta charset="UTF-8">\n  <title>${1:Document Title}</title>\n</head>\n<body>\n ${2}\n</body>\n</html>',
-            description: 'HTML5 Template'
-          }]
-        });
-// script Monaco in-site function updatePreview -------------------------------------------------
 
-        function updatePreview() {
-          var preview = document.getElementById('preview');
-          var previewDoc = preview.contentDocument || preview.contentWindow.document;
-          previewDoc.open();
-          previewDoc.write(editor.getValue());
-          previewDoc.close();
+    // localstorage inside editor to keep code when reloading page
+    var editorValue = localStorage.getItem('editorValue');
+    var editor = monaco.editor.create(document.getElementById('editor'), {
+      value: editorValue || '<!DOCTYPE html>\n<html>\n<head>\n  <meta charset="UTF-8">\n  <title>My Test Page</title>\n<style>body{color:white;}</style>\n</head>\n<body> \n</body>\n</html>',
+      language: 'html',
+      theme: 'dark-theme',
+      automaticLayout: true,
+      suggestOnTriggerCharacters: true, // enable auto-completion on trigger characters
+      autoClosingBrackets: true, // enable auto-closing brackets
+      autoClosingQuotes: true, // enable auto-closing quotes
+      autoIndent: true, // enable auto-indent
+      snippets: [
+        {
+          label: 'HTML5 Template', // HTML5 template snippet
+          body: '<!DOCTYPE html>\n<html>\n<head>\n  <meta charset="UTF-8">\n  <title>${1:Document Title}</title>\n</head>\n<body>\n ${2}\n</body>\n</html>',
+          description: 'HTML5 Template'
         }
+      ]
+    });
 
-// script Monaco in-site preview keeping when type code in editor --------------------------------
-        editor.onDidChangeModelContent(function(event) {
-          var value = editor.getValue();
-          localStorage.setItem('editorValue', value);
-          updatePreview();
-        });
+    // Update preview on editor change
+    function updatePreview() {
+      var preview = document.getElementById('preview');
+      var previewDoc = preview.contentDocument || preview.contentWindow.document;
+      previewDoc.open();
+      previewDoc.write(editor.getValue());
+      previewDoc.close();
+    }
+    // Save editor value to local storage and update preview
+    function saveEditorValue() {
+      var value = editor.getValue();
+      localStorage.setItem('editorValue', value);
+      updatePreview();
+    }
 
-        updatePreview();
-      });
+    // Event listener for editor content change
+    editor.onDidChangeModelContent(function(event) {
+      saveEditorValue();
+    });
+
+    // Undo the last change
+    function undo() {
+      editor.trigger('keyboard', 'undo', null);
+    }
+
+    // Redo the last undone change
+    function redo() {
+      editor.trigger('keyboard', 'redo', null);
+    }
+
+    // Attach undo and redo functions to buttons or events
+    var undoButton = document.getElementById('undoButton');
+    undoButton.addEventListener('click', undo);
+
+    var redoButton = document.getElementById('redoButton');
+    redoButton.addEventListener('click', redo);
+
+
+    updatePreview();
+  });
 
 // FUNCTIONAL HTMML ONLY END --------------------------------
 
