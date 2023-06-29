cce5102d03fae14240956b3a3420ee89e940c372
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-06-29 21:27:46 +0200

    gig

--- resources/js/components/MyBlogContent.vue
+++ resources/js/components/MyBlogContent.vue
@@ -7,7 +7,7 @@
             data-aos="flip-left">
             <div class="textfieldBlogTitle mx-2 px-0">
                 <h3>{{ blog.title }}</h3>
-                <p>{{ blog.user_id }}</p>
+                <p>{{ blog.user.name }}</p>
                 <img
                     :src="getImageUrl(blog.image)"
                     class="img-fluid imageBlog"
