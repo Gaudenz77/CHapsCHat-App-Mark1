8d2bf17320dce93582746059c2cab876302c0bd0
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-06-30 14:32:28 +0200

    beautufy

--- resources/js/components/MyBlogForm.vue
+++ resources/js/components/MyBlogForm.vue
@@ -1,108 +1,176 @@
 <template>
     <div class="mx-3">
         <form @submit.prevent="submitForm" enctype="multipart/form-data">
-
             <label class="form-label formOwnOne" for="title"></label>
             <div class="input-group mb-4">
                 <span class="input-group-text formOwnOne" id="basic-addon1">
-                    <i class="fa-solid fa-palette formIcons" 
-                        data-bs-custom-class="custom-tooltip" 
-                        data-bs-toggle="tooltip" 
+                    <i
+                        class="fa-solid fa-palette formIcons"
+                        data-bs-custom-class="custom-tooltip"
+                        data-bs-toggle="tooltip"
                         data-bs-placement="top"
-                        data-bs-html="true" 
-                        title="Select<br>a <br>title"></i></span>
-                <input type="text" name="title" class="form-control formOwnOne" placeholder="Title" id="title" v-model="form.title" required>
+                        data-bs-html="true"
+                        title="Select<br>a <br>title"
+                    ></i
+                ></span>
+                <input
+                    type="text"
+                    name="title"
+                    class="form-control formOwnOne"
+                    placeholder="Title"
+                    id="title"
+                    v-model="form.title"
+                    required
+                />
             </div>
 
             <label class="form-label" for="topic"></label>
             <div class="input-group mb-4">
                 <span class="input-group-text formOwnOne">
-                    <i class="fa-solid fa-book-bookmark formIcons" 
-                        data-bs-custom-class="custom-tooltip" 
-                        data-bs-toggle="tooltip" 
+                    <i
+                        class="fa-solid fa-book-bookmark formIcons"
+                        data-bs-custom-class="custom-tooltip"
+                        data-bs-toggle="tooltip"
                         data-bs-placement="top"
-                        data-bs-html="true" 
-                        title="Select <br> a <br> topic"></i></span>
-                <select class="form-control formOwnOne" id="topic" v-model="form.topic" required>
-                    <option value="general"> General</option>
+                        data-bs-html="true"
+                        title="Select <br> a <br> topic"
+                    ></i
+                ></span>
+                <select
+                    class="form-control formOwnOne"
+                    id="topic"
+                    v-model="form.topic"
+                    required
+                >
+                    <option value="general">General</option>
                     <option value="resources">Resources</option>
                     <option value="techtalk">Tech Talk</option>
-                    <option data-content="<i class='fa-solid fa-heart' aria-hidden='true'></i>">Option3</option>
+                    <option
+                        data-content="<i class='fa-solid fa-heart' aria-hidden='true'></i>"
+                    >
+                        Option3
+                    </option>
                 </select>
             </div>
 
             <label class="form-label formOwnOne" for="content"></label>
             <div class="input-group mb-4">
-                <span class="input-group-text formOwnOne"><i class="fa-solid fa-file-signature formIcons" data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip" data-bs-placement="top" title="Enter your content"></i></span>
-                <textarea class="form-control formOwnOne" placeholder="Content" name="content" id="content" rows="5" v-model="form.content" required></textarea>
+                <span class="input-group-text formOwnOne"
+                    ><i
+                        class="fa-solid fa-file-signature formIcons"
+                        data-bs-custom-class="custom-tooltip"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="Enter your content"
+                    ></i
+                ></span>
+                <textarea
+                    class="form-control formOwnOne"
+                    placeholder="Content"
+                    name="content"
+                    id="content"
+                    rows="5"
+                    v-model="form.content"
+                    required
+                ></textarea>
             </div>
 
             <div class="input-group mb-4">
-                <input type="file" name="image" class="form-control formOwnOne" accept="image/*" @change="onFileChange">
+                <input
+                    type="file"
+                    name="image"
+                    class="form-control formOwnOne"
+                    accept="image/*"
+                    @change="onFileChange"
+                />
             </div>
 
-            <input type="hidden" name="_token" :value="form.csrfToken">
-            
+            <input type="hidden" name="_token" :value="form.csrfToken" />
+
             <div id="sendLibrary" class="text-center pt-0">
-                <button type="submit" id="" class="btn btn-circle mt-2 mb-2 p-1" v-html="editMode ? '<i class=\'fa-regular fa-edit\'></i> Update' : '<i class=\'fa-regular fa-save fa-2xl\'></i><br>'"></button>
-            </div>                                                           
+                <button
+                    type="submit"
+                    id=""
+                    class="btn btn-circle mt-2 mb-2 p-1"
+                    v-html="
+                        editMode
+                            ? '<i class=\'fa-regular fa-edit\'></i> Update'
+                            : '<i class=\'fa-regular fa-save fa-2xl\'></i><br>'
+                    "
+                ></button>
+            </div>
         </form>
 
-        <div v-if="successMessage" class="alert alert-success alert-dismissible fade show">
-        {{ successMessage }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <div
+            v-if="successMessage"
+            class="alert alert-success alert-dismissible fade show"
+        >
+            {{ successMessage }}
+            <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="alert"
+                aria-label="Close"
+            ></button>
         </div>
-        <button type="button" id="closeButton2" class="btn-close" aria-label="Close"></button>
-    <p>Close Create Blog</p>
+        <button
+            type="button"
+            id="closeButton2"
+            class="btn-close"
+            aria-label="Close"
+        ></button>
+        <p>Close Create Blog</p>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-  data() {
-    return {
-      form: {
-        title: '',
-        topic: 'general',
-        content: '',
-        image: null,
-        csrfToken: document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-      },
-      successMessage: '', // Variable to store the success message
-    };
-    
-  },    
-  methods: {
-    onFileChange(event) {
-      this.form.image = event.target.files[0];
+    data() {
+        return {
+            form: {
+                title: "",
+                topic: "general",
+                content: "",
+                image: null,
+                csrfToken: document
+                    .querySelector('meta[name="csrf-token"]')
+                    .getAttribute("content"),
+            },
+            successMessage: "", // Variable to store the success message
+        };
     },
-    submitForm() {
-      axios.post('/blogosphere', this.form, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
+    methods: {
+        onFileChange(event) {
+            this.form.image = event.target.files[0];
         },
-      })
-        .then(response => {
-          // Handle success
-          console.log(response.data);
-          // Reset the form fields
-          this.form.title = '';
-          this.form.topic = 'general';
-          this.form.content = '';
-          this.form.image = null;
-          // Set the success message
-          this.successMessage = response.data.message;
-          // Reload the page after a delay
-          setTimeout(() => {
-            location.reload();
-          }, 2000); // Adjust the delay time (in milliseconds) as needed
-        })
-        .catch(error => {
-          console.log("ERRRR:: ", error.response.data);
-        });
+        submitForm() {
+            axios
+                .post("/blogosphere", this.form, {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                })
+                .then((response) => {
+                    // Handle success
+                    console.log(response.data);
+                    // Reset the form fields
+                    this.form.title = "";
+                    this.form.topic = "general";
+                    this.form.content = "";
+                    this.form.image = null;
+                    // Set the success message
+                    this.successMessage = response.data.message;
+                    // Reload the page after a delay
+                    setTimeout(() => {
+                        location.reload();
+                    }, 2000); // Adjust the delay time (in milliseconds) as needed
+                })
+                .catch((error) => {
+                    console.log("ERRRR:: ", error.response.data);
+                });
+        },
     },
-  },
 };
 </script>
--- resources/views/blogoSphere.blade.php
+++ resources/views/blogoSphere.blade.php
@@ -1,79 +1,59 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 @extends('layouts.master')
 
-@section('title', 'ChapsChat')
+@section('title', 'ChapsChat') 
 
 @section('body-class', 'blogMain')
 
-@section('content')
-
-<section class="">
-    <div class="container-fluid">
-
-        <div class="row justify-content-center align-items-middle">
-            <div class="col-sm-4 col-lg-3 columnBlogTwo px-lg-4 px-2 pt-4 mb-0" id="formContainer" style="display: none;">
-                <div class="form-wrapper">
-                  @auth
-                  <h1 class="">Create Blog</h1>
-                  <my-blog-form></my-blog-form>
-                  @endauth 
-                </div>
-            </div>
-            
-            <div class="col-sm-8 col-lg-8 colBlogContent mt-md-0 mt-0">
-                <div class="container mb-3" style=" margin-top:-15% ! important">
-                  <div class="row">
-                    @auth
-                    <div class="col-12 text-center mt-3 mb-2">
-                        <button class="btn btn-own mt-md-0 mt-3 text-end" type="button" role="button" id="createButton2">
-                          <i class="fa-solid fa-circle-plus fa-4x formOwnOne pt-2"
-                              data-bs-custom-class="custom-tooltip"
-                              data-bs-toggle="tooltip"
-                              data-bs-placement="left"
-                              data-bs-html="true"
-                              title="<b>Press to<br>open form</b>">
-                          </i></button>
-                        <p class="pBlogText small mt-0 mb-0" style="padding-top:-2rem;">Create</p>
-                    </div>
-                    @endauth
-                  </div>
-                    
-                    <div class="row">
-                      <my-blog-content :auth-user-id="{{ Auth::check() ? Auth::id() : 'null' }}"></my-blog-content>
-                    </div>
-                </div>
-            </div>
+@section('content') <section class="">
+  <div class="container-fluid">
+    <div class="row justify-content-center align-items-middle">
+      <div class="col-sm-4 col-lg-3 columnBlogTwo px-lg-4 px-2 pt-4 mb-0" id="formContainer" style="display: none;">
+        <div class="form-wrapper"> @auth <h1 class="">Create Blog</h1>
+          <my-blog-form></my-blog-form> @endauth
         </div>
+      </div>
+      <div class="col-sm-8 col-lg-8 colBlogContent mt-md-0 mt-0">
+        <div class="container mb-3" style=" margin-top:-15% ! important">
+          <div class="row"> @auth <div class="col-12 text-center mt-3 mb-2">
+              <button class="btn btn-own mt-md-0 mt-3 text-end" type="button" role="button" id="createButton2">
+                <i class="fa-solid fa-circle-plus fa-4x formOwnOne pt-2" data-bs-custom-class="custom-tooltip" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-html="true" title="
+									<b>Press to
+										<br>open form
+										</b>">
+                </i>
+              </button>
+              <p class="pBlogText small mt-0 mb-0" style="padding-top:-2rem;">Create</p>
+            </div> @endauth </div>
+          <div class="row">
+            <my-blog-content :auth-user-id="{{ Auth::check() ? Auth::id() : 'null' }}"></my-blog-content>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </section>
 
-
-@endsection
+@endsection 
 
 <script>
-
-    // CREATE BLOG DIV -------------------------------------------------------TEST IF ENTRY PAGE STILL WORKING!!!
-
-document.addEventListener('DOMContentLoaded', () => {
-  const createButton = document.getElementById('createButton2');
-const formContainer = document.getElementById('formContainer');
-
-createButton.addEventListener('click', () => {
-  formContainer.style.display = 'block';
+  // CREATE BLOG DIV -------------------------------------------------------TEST IF ENTRY PAGE STILL WORKING!!!
+  document.addEventListener('DOMContentLoaded', () => {
+    const createButton = document.getElementById('createButton2');
+    const formContainer = document.getElementById('formContainer');
+    createButton.addEventListener('click', () => {
+      formContainer.style.display = 'block';
+    });
   });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-  const createButton = document.getElementById('createButton2');
-  const formContainer = document.getElementById('formContainer');
-  const closeButton = document.getElementById('closeButton2');
-
-  createButton.addEventListener('click', () => {
-    formContainer.style.display = 'block';
+  document.addEventListener('DOMContentLoaded', () => {
+    const createButton = document.getElementById('createButton2');
+    const formContainer = document.getElementById('formContainer');
+    const closeButton = document.getElementById('closeButton2');
+    createButton.addEventListener('click', () => {
+      formContainer.style.display = 'block';
+    });
+    closeButton.addEventListener('click', () => {
+      formContainer.style.display = 'none';
+    });
   });
-
-  closeButton.addEventListener('click', () => {
-    formContainer.style.display = 'none';
-  });
-});
 </script>
