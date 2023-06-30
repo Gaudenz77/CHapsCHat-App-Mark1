27fee0c062cecacba8e22cfde51f396ad20a66c9
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-06-30 23:03:27 +0200

    gogo

--- resources/js/components/MyBlogContent.vue
+++ resources/js/components/MyBlogContent.vue
@@ -24,8 +24,10 @@
           <div class="modal-dialog modal-xl">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title">{{ blog.title }}</h5>
-                <button type="button" class="btn-close btnXModal" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div class="row">
+                  <div class="col"><h5 class="modal-title">{{ blog.title }}</h5></div>
+                  <div class="col text-end"><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div>
+                </div>
               </div>
               <div class="modal-body">
                 <p>
