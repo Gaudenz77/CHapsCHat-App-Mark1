abec138808644f421526b6eaa66a6c99c3d2a382
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-06-30 12:22:51 +0200

    gogo

--- resources/js/components/MyBlogContent.vue
+++ resources/js/components/MyBlogContent.vue
@@ -15,7 +15,7 @@
             alt="Blog Image"
           />
           <div class="row justify-content-center">
-            <div v-if="isAuthor(blog)" class="col-5 px-0 mt-4">
+            <div v-if="isAuthor(blog.user_id)" class="col-5 px-0 mt-4">
               <button
                 class="btn btn-circleLibrary mt-2 mb-2 p-1"
                 type="button"
@@ -39,90 +39,118 @@
             </div>
           </div>
         </div>
-  
-        <!-- Modal -->
-        <div
-          class="modal fade"
-          :id="'modal-' + blog.id"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-          tabindex="-1"
-        >
-          <!-- Modal content -->
+
+                <!-- Modal -->
+                <div
+                    class="modal fade"
+                    :id="'modal-' + blog.id"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                    tabindex="-1">
+                    <div class="modal-dialog modal-xl">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">{{ blog.title }}</h5>
+                                <button
+                                    type="button"
+                                    class="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p><strong>User ID:</strong> {{ blog.user_id }}</p>
+                                <!-- <p><strong>Topic:</strong> {{ blog.topic }}</p> -->
+                                <p class="textfieldBlogContent">
+                                    {{ blog.content }}
+                                </p>
+                                <img
+                                    :src="getImageUrl(blog.image)"
+                                    class="img-fluid imageBlog"
+                                    alt="Blog Image"/>
+                            </div>
+                            <div class="modal-footer">
+                                <button
+                                    type="button"
+                                    class="btn btn-circleLibrary"
+                                    data-bs-dismiss="modal">
+                                    Close
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import axios from "axios";
-  import AOS from "aos";
-  import "aos/dist/aos.css";
-  
-  export default {
-    props: ["authUserId"],
-    data() {
-      return {
-        blogs: [],
-        loggedUser: document
-          .querySelector("meta[name='user']")
-          .getAttribute("content"),
-      };
+    </template>
+
+<script>
+import axios from "axios";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export default {
+  props: ["authUserId"],
+  data() {
+    return {
+      blogs: [],
+      loggedUser: document
+        .querySelector("meta[name='user']")
+        .getAttribute("content"),
+    };
+  },
+  computed: {
+    reversedMessages() {
+      return this.messages.slice().reverse();
     },
-    computed: {
-      reversedMessages() {
-        return this.messages.slice().reverse();
-      },
-      authUserId() {
-        return this.$props.authUserId;
-      },
+    authUserId() {
+      return this.$props.authUserId;
     },
-    mounted() {
-      this.fetchBlogs();
-      console.log(this.loggedUser);
-      AOS.init(); // Initialize AOS
+  },
+  mounted() {
+    this.fetchBlogs();
+    console.log(this.loggedUser);
+    AOS.init(); // Initialize AOS
+  },
+  methods: {
+    fetchBlogs() {
+      axios
+        .get("/blogosphere")
+        .then((response) => {
+          this.blogs = response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    methods: {
-      fetchBlogs() {
-        axios
-          .get("/blogosphere")
-          .then((response) => {
-            this.blogs = response.data;
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      },
-      getImageUrl(image) {
-        if (image) {
-          const baseUrl = "/assets/img/"; // Update the base URL to match your folder structure
-          return baseUrl + image;
-        }
-        // Return a placeholder image URL or an empty string if no image is available
-        return "/assets/img/ChapsChatLogo.png";
-      },
-      deleteBlog(id) {
-        const csrfToken = document
-          .querySelector('meta[name="csrf-token"]')
-          .getAttribute("content");
-  
-        axios
-          .delete(`/blogosphere/${id}`, {
-            headers: {
-              "X-CSRF-TOKEN": csrfToken,
-            },
-          })
-          .then((response) => {
-            this.blogs = this.blogs.filter((blog) => blog.id !== id);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      },
-      isAuthor(blog) {
-        return blog.user_id === this.loggedUser;
-      },
+    getImageUrl(image) {
+      if (image) {
+        const baseUrl = "/assets/img/"; // Update the base URL to match your folder structure
+        return baseUrl + image;
+      }
+      // Return a placeholder image URL or an empty string if no image is available
+      return "/assets/img/ChapsChatLogo.png";
     },
-  };
-  </script>
-  
+    deleteBlog(id) {
+      const csrfToken = document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content");
+
+      axios
+        .delete(`/blogosphere/${id}`, {
+          headers: {
+            "X-CSRF-TOKEN": csrfToken,
+          },
+        })
+        .then((response) => {
+          this.blogs = this.blogs.filter((blog) => blog.id !== id);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    isAuthor(userId) {
+      return userId === this.loggedUser;
+    },
+  },
+};
+</script>
