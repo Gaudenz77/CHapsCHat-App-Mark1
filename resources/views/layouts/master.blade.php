c616be45a0d805ae780d386e8983eaa419994d82
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-07-25 15:21:20 +0200

    Google Site Verification metatag

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="mOdZWR6MamPSqqjIn4cerD4DjI5409y8pPXtHgyICjw" />
     {{-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> --}}
 
     <!-- Cross Server Reference token setting  -->
